d4c2cb0fbde9dfc3f5538d50b21020b500f29ec4
Nikolaus, Fynn <fynn.nikolaus@chg-meridian.com>
2019-09-04 14:20:09 +0200

    Initial commit

---
+++ GameCatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("..........................................................");
+        }
+    }
+}
