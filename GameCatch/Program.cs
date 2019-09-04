fc7c26dd7e0b755adbd933c03e10e929379befee
Nikolaus, Fynn <fynn.nikolaus@chg-meridian.com>
2019-09-04 16:20:23 +0200

    frame in a function

--- GameCatch/Program.cs
+++ GameCatch/Program.cs
@@ -6,7 +6,29 @@ namespace GameCatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("..........................................................");
+            showlayout();   
+
+            var input = Console.ReadLine();
+            Console.WriteLine("you have entered: " + input);
+
+            showlayout();
+
+        }
+
+        static void showlayout()
+        {
+            Console.WriteLine("                          ");
+            Console.WriteLine("   # # # # # # # # # # # #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   #                     #");
+            Console.WriteLine("   # # # # # # # # # # # #");
         }
     }
 }
