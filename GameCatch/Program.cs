f06193844241648d2d8a23044bed63d4fe1d7db9
Nikolaus, Fynn <fynn.nikolaus@chg-meridian.com>
2019-09-09 12:56:16 +0200

    new for

--- GameCatch/Program.cs
+++ GameCatch/Program.cs
@@ -17,12 +17,30 @@ namespace GameCatch
             }
             Console.WriteLine("\u2557");
 
+            
+
             for (int i = 0; i < Size; i++) 
             {
-                Console.WriteLine("   \u2551                    \u2551");
+                Console.Write("   \u2551");
+                // Console.WriteLine("   \u2551\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u2551");
+                for (int j = 0; j < (Size * 2); j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\u2551");
             }
+
+
+            Console.Write("   \u255A");
+            for (int i = 0; i < (Size * 2); i++)
+            {
+                Console.Write("\u2550");
+            }
+            Console.WriteLine("\u255D");
            
-            //Console.WriteLine("   \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
+            
+            
+            
         }
 
         
