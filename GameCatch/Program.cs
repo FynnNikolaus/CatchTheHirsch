cafab41b0016a9860fdab11a239cc66e40c0db86
Nikolaus, Fynn <fynn.nikolaus@chg-meridian.com>
2019-09-09 10:57:18 +0200

    better

--- GameCatch/Program.cs
+++ GameCatch/Program.cs
@@ -4,62 +4,41 @@ namespace GameCatch
 {
     class Program
     {
-        static void showlayout(string[] args)
+        static void Main(string[] args)
         {
+            
+            var Size = 10; // Matchfield size
             Console.WriteLine("");
-            Console.WriteLine("                          ");
-            Console.WriteLine("   \u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
-            for (int A = 0; A < 9; A++) // Frame length 9 + \u2554 = size 10
-            {
-                Console.WriteLine("   \u2551                     \u2551");
-            }
-            Console.WriteLine("   \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
-
-            
-        }
-            
+            Console.Write("   \u2554");
            
-            
+            for (int i = 0; i < (Size*2); i++)
+            {                  
+                Console.Write("\u2550");
+            }
+            Console.WriteLine("\u2557");
 
+            for (int i = 0; i < Size; i++) 
+            {
+                Console.WriteLine("   \u2551                    \u2551");
+            }
+           
+            //Console.WriteLine("   \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
+        }
 
         
-       }
-
-        static void KeyReading()
-        {
-            var MovementInput = Console.ReadKey();
-
-            if (MovementInput.Key == ConsoleKey.W)
-            {
-                Console.Clear(); // Erneuerung
-                Console.WriteLine("                          ");
-                Console.WriteLine("   \u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557");
-                Console.WriteLine("   \u2551                     \u2551");
-                Console.WriteLine("   \u2551                     \u2551");
-                Console.WriteLine("   \u2551                     \u2551");
-                Console.WriteLine("   \u2551                     \u2551");
-                Console.WriteLine("   \u2551                     \u2551");
-                Console.WriteLine("   \u2551                     \u2551");
-                Console.WriteLine("   \u2551                     \u2551");
-                Console.WriteLine("   \u2551 1                   \u2551");
-                Console.WriteLine("   \u2551                     \u2551");
-                Console.WriteLine("   \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D");
-
-            }
-
-            while (MovementInput.Key != ConsoleKey.W)
-            {
-                Console.WriteLine("Please press a valid button!");
-                break;  
-            }
-
-        }
-
-
-
 
 
 
 
     }
+
+    
+
+
+
+
+
+
+
 }
+
