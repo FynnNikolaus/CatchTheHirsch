31f7124135d4695a0f5332f72eb855569fbc4481
Nikolaus, Fynn <fynn.nikolaus@chg-meridian.com>
2019-09-10 09:15:18 +0200

    MANU bar ready

--- GameCatch/Program.cs
+++ GameCatch/Program.cs
@@ -17,51 +17,58 @@ namespace GameCatch
             Console.WriteLine("");
             Console.WriteLine(" Welcome to catchTheHirsch MENU BAR");
             Console.WriteLine(" Press F1 to START or press F2 to CLOSE");
-            var MENU
-            while ()
-            var Size = 10;                                          // Matchfield size
-            Console.WriteLine("");
-            Console.Write("   " + TopLeftCorner);
-           
-            for (int i = 0; i < (Size*2); i++)
-            {                  
-                Console.Write(HorizontalLine);
-            }
-
-            Console.WriteLine(TopRightCorner);
-
-            for (int i = 0; i < Size; i++) 
+            var MENU = Console.ReadKey();
+            if (MENU.Key == ConsoleKey.F1)
             {
-                Console.Write("   " + VerticalLine);
-                
-                for (int j = 0; j < (Size * 2); j++)
+                Console.WriteLine("");
+                Console.WriteLine(" Please enter your nickname: ");
+                Console.Write(" Player 1:");
+                Console.ReadLine();
+                Console.Write(" Player 2:");
+                Console.ReadLine();
+                var Size = 10;                                          // Matchfield size
+                Console.WriteLine("");
+                Console.Write("   " + TopLeftCorner);
+
+                for (int i = 0; i < (Size * 2); i++)
                 {
-                    Console.Write(" ");
+                    Console.Write(HorizontalLine);
                 }
 
-                Console.WriteLine(VerticalLine);
+                Console.WriteLine(TopRightCorner);
+
+                for (int i = 0; i < Size; i++)
+                {
+                    Console.Write("   " + VerticalLine);
+
+                    for (int j = 0; j < (Size * 2); j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    Console.WriteLine(VerticalLine);
+                }
+
+                Console.Write("   " + BelowLeftCorner);
+
+                for (int i = 0; i < (Size * 2); i++)
+                {
+                    Console.Write(HorizontalLine);
+                }
+
+                Console.WriteLine(BelowRightCorner);
             }
 
-            Console.Write("   " + BelowLeftCorner);
-
-            for (int i = 0; i < (Size * 2); i++)
+            if (MENU.Key == ConsoleKey.F2)
             {
-                Console.Write(HorizontalLine);
+
+                System.Environment.Exit(0);
             }
 
-            Console.WriteLine(BelowRightCorner);
-
-            //Player input 
-
-            Console.Write("   Player 1:");
-            Console.ReadLine();
-            Console.Write("   Player 2:");
-            Console.ReadLine();
+           
+           
             
             
-
-
-
 
 
         }
