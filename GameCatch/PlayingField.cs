7cdbcd6ab290598de1173caeccf44d202aef1600
Nikolaus, Fynn <fynn.nikolaus@chg-meridian.com>
2019-12-12 11:32:51 +0100

    New class Playingfield

---
+++ GameCatch/PlayingField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCatch
+{
+    class PlayingField
+    {
+        public Position position;
+        enum MoveResult
+        {
+            position,
+
+
+        }
+        public Position GetPlayerPosition(HumanPlayer player)
+        {
+            return position;
+        }
+    }
+}
